6c3ee0ed042e226105541de2d965d64b7d2a7cd8
prafulpatel16 <praful1611@outlook.com>
2022-12-29 23:57:58 -0600

    region error fixed

--- terraform-manifests/backend-dev.tf
+++ terraform-manifests/backend-dev.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     encrypt = true    
-    bucket = "backend-dev"
-    dynamodb_table = "terraform-state-lock-dynamo"
+    bucket = "backend-dev-20231611"
+    dynamodb_table = "terraform-state-lock-dynamo-1611"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
